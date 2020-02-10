9ab32637205188c0f965b7483b0c31147b1a5700
Humberto Hernandez <hamburger45@gmail.com>
2020-02-10 00:08:56 -0800

    modified for gh pages

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const testString = 'connected to data.js';
+
+const keywords = {
+	'all in quality': true,
+	'all-in-quality': true,
+	aiq: true,
+	quality: true,
+	'dispatch efficiency': true,
+	de: true,
+	efficiency: true,
+	eff: true,
+	voc: true,
+	'voice of the customer': true,
+	crift: true,
+	'cr!ft': true,
+	'effective performance': true,
+	ep: true,
+	attainment: true,
+	coaching: true,
+	safe: true,
+	safety: true,
+	ride: true,
+	discipline: true,
+	perf: true
+};
+
+let textAreaArray = [];
+
+let textObj = {};
+
+// test case use
+// let keywordCount = 0;
+
+let sortedKeywords = [];
 // test message remove for production
 function startMessage() {
 	console.log('Script Connected!');
